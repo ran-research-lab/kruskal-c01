322aaa08778fd9d0cd674acba9bc395d30c5150e
Rafael Arce Nazario <rafael.arce@upr.edu>
2020-12-06 12:17:21 -0400

    initial

---
+++ graph.cpp
@@ -0,0 +1,34 @@
+#include "graph.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+// We define this function to be able to call on the sort function
+// for a vector of WeightedEdges.
+bool WeightedEdge::operator<(const WeightedEdge &lSide) const {
+    return w < lSide.w;
+}
+
+void Graph::addWeightedEdge(int a, int b, int w) {
+    edgeList.push_back(WeightedEdge(a,b,w));
+}
+
+
+
+// function minSpanningTree:
+// Implements Kruskal's minimum spanning tree algorithm and outputs
+// the list of edges of the min spanning tree.
+
+void Graph::minSpanningTree() {
+
+    // This will sort the edges in ascending weight order
+    sort(edgeList.begin(), edgeList.end());
+
+    // Uncomment to see the edges in ascending order of weight
+    for (auto e: edgeList) cout << e.a << " " << e.b << " " << e.w << endl;
+
+
+}
---
+++ graph.h
@@ -0,0 +1,36 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class WeightedEdge{
+public:
+    // a and b are the joined vertices, w is the weight of the edge
+    int a, b, w;
+    WeightedEdge()  {}
+    WeightedEdge(int aa, int bb, int ww) : a(aa),b(bb),w(ww) {}
+    void set(int aa, int bb, int ww) { a = aa; b = bb; w = ww; }
+    bool operator<(const WeightedEdge &lSide) const;
+};
+
+
+class Graph {
+private:
+    vector<WeightedEdge> edgeList;
+
+public:
+
+    Graph() { edgeList.resize(0); }
+    void addWeightedEdge(int aa, int bb, int w);
+
+    void minSpanningTree();
+};
+
+
+
+
+#endif
+
